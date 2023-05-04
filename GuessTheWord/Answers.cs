e04cd50d51b5e9b366611b6f290c58bb98147773
Jon <hilton.jon@gmail.com>
2023-05-04 13:55:33 +0100

    Committed missing Films helper

---
+++ GuessTheWord/Answers.cs
@@ -0,0 +1,33 @@
+﻿namespace GuessTheWord;
+
+public static class Films
+{
+    private static readonly List<string> _answerList = new()
+    {
+        "Mission Impossible",
+        "The Matrix",
+        "Terminator",
+        "Star Trek",
+        "Star Wars",
+        "The Lord of the Rings",
+        "The Hobbit",
+        "Harry Potter",
+        "The Avengers",
+        "The Lion King",
+        "Alien",
+        "Men in Black",
+        "Galaxy Quest",
+        "Ghostbusters",
+        "Back to the Future",
+        "Indiana Jones",
+        "Spiderman",
+        "Superman",
+        "Wonder Woman"
+
+    };
+    
+    public static string Random()
+    {
+        return _answerList[new Random().Next(0, _answerList.Count)].ToUpper();
+    }
+}
